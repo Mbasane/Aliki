0d0c95ac130aeaabce6e88a6a9a436dcff065af0
Vuyani Daweti <vuyanidwt@gmail.com>
2023-10-04 16:59:12 +0200

    fixed validation and commented the console.logs

--- script.js
+++ script.js
@@ -12,22 +12,20 @@ document.getElementById("contactForm").addEventListener("submit", function (even
 {
   event.preventDefault();
 
-    // Retrieve the input field values
-    var firstName = document.getElementById("firstNameInput");
-    var lastName = document.getElementById("lastNameInput");
-    var email = document.getElementById("emailInput");
-    var contactNumber = document.getElementById("contactNumberInput");
-    var request = document.getElementById("requestInput");
+  // Retrieve the input field values
+  var firstName = document.getElementById("firstNameInput");
+  var lastName = document.getElementById("lastNameInput");
+  var email = document.getElementById("emailInput");
+  var contactNumber = document.getElementById("contactNumberInput");
+  var request = document.getElementById("requestInput");
 
-    // Function to add error styles and messages to the input fields
-    function setErrorStyles(field, message) {
+  // Function to add error styles and messages to the input fields
+  function setErrorStyles(field, message) {
+    var existingErrorMessage = field.parentNode.querySelector(".error-message");
 
-      var existingErrorMessage = field.parentNode.querySelector('.error-message');
-      
-      if(existingErrorMessage)
-      {
-        existingErrorMessage.textContent = message;
-      } else {
+    if (existingErrorMessage) {
+      existingErrorMessage.textContent = message;
+    } else {
       field.classList.add("error");
       var errorMessage = document.createElement("div");
       errorMessage.className = "error-message";
@@ -36,78 +34,93 @@ document.getElementById("contactForm").addEventListener("submit", function (even
     }
   }
 
-    // Function to remove error styles and messages from the input fields
-    function removeErrorStyles(field) {
-        field.classList.remove("error");
-        var existingErrorMessage = field.parentNode.querySelector('.error-message');
-        if (existingErrorMessage) 
-        {
-          existingErrorMessage.parentNode.removeChild(existingErrorMessage);
-        }
-      }
-
-    // Check if the first name is empty
-    if (firstName.value.trim() === "") {
-      setErrorStyles(firstName, "Please enter your first name");
-    } else {
-      removeErrorStyles(firstName);
+  // Function to remove error styles and messages from the input fields
+  function removeErrorStyles(field) {
+    field.classList.remove("error");
+    var existingErrorMessage = field.parentNode.querySelector(".error-message");
+    if (existingErrorMessage) {
+      existingErrorMessage.parentNode.removeChild(existingErrorMessage);
     }
+  }
 
-    // Validation for first name
-    var firstNameCheck = /^[A-Za-z]+$/;
-    if (!firstNameCheck.test(firstName.value)) {
-      setErrorStyles(
-        firstName,
-        "First name must contain only alphabetic characters.");
-      } else {
-        removeErrorStyles(firstName);
-    }
+  // Check if the first name is empty
+  if (firstName.value.trim() === "") {
+    setErrorStyles(firstName, "Please enter your first name");
+  } else {
+    removeErrorStyles(firstName);
+  }
 
-    // Check if the last name is empty
-    if (lastName.value.trim() === "") {
-      setErrorStyles(lastName, "Please enter your last name");
-    } else {
-      removeErrorStyles(lastName);
-    }
-    
-    // Validation for last name
-    var lastNameCheck = /^[A-Za-z]+$/;
-    if (!lastNameCheck.test(lastName.value)) {
-      setErrorStyles(lastName, "Last name must contain only alphabetic characters.");
-      } else {
-        removeErrorStyles(lastName);
-      }
+  // Validation for first name
+  var firstNameCheck = /^[A-Za-z]+$/;
+  if (!firstNameCheck.test(firstName.value)) {
+    setErrorStyles(
+      firstName,
+      "First name must contain only alphabetic characters."
+    );
+  } else {
+    removeErrorStyles(firstName);
+  }
 
-    // Validation for email format
-    var emailCheck = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailCheck.test(email.value)) {
-      setErrorStyles(email, "Please enter a valid email address.");
-      } else {
-        removeErrorStyles(email);
-      }
+  // Check if the last name is empty
+  if (lastName.value.trim() === "") {
+    setErrorStyles(lastName, "Please enter your last name");
+  } else {
+    removeErrorStyles(lastName);
+  }
 
-    // Validation for contact number
-    var contactNumberCheck = /^\d+$/;
-    if (!contactNumberCheck.test(contactNumber.value)) {
-      setErrorStyles(contactNumber, "Contact number must contain only numbers");
-    } else {
-      removeErrorStyles(contactNumber);
-    }
-    
-    // Check if the request field is empty
-    if (request.value.trim() === '') {
-      setErrorStyles(request, 'Please describe your request');
-    } else {
-      removeErrorStyles(request);
-    }
+  // Validation for last name
 
-    console.log("Form data:");
-    console.log("First Name:", firstName.value);
-    console.log("Last Name:", lastName.value);
-    console.log("Email:", email.value);
-    console.log("Contact Number:", contactNumber.value);
-    console.log("Request:", request.value);
-  });
+  var lastNameCheck = /^[A-Za-z]+$/;
+  if (!lastNameCheck.test(lastName.value)) {
+    setErrorStyles(
+      lastName,
+      "Last name must contain only alphabetic characters."
+    );
+  } else {
+    removeErrorStyles(lastName);
+  }
+
+  // Validation for email format
+  var emailCheck = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailCheck.test(email.value)) {
+    setErrorStyles(email, "Please enter a valid email address.");
+  } else {
+    removeErrorStyles(email);
+  }
+
+  // Validation for contact number
+  var contactNumberCheck = /^\d+$/;
+  // if (
+  //   !contactNumberCheck.test(contactNumber.value) &&
+  //   contactNumber.value.length !== 10
+  // ) {
+  //   setErrorStyles(contactNumber, "Contact number must contain only numbers");
+  // } else {
+  //   removeErrorStyles(contactNumber);
+  // }
+  if (isNaN(contactNumber.value) || contactNumber.value.length !== 10) {
+    setErrorStyles(
+      contactNumber,
+      "Contact number must contain only numbers , and must be 10 digits (exclude country code)"
+    );
+  } else {
+    removeErrorStyles(contactNumber);
+  }
+
+  // Check if the request field is empty
+  if (request.value.trim() === "") {
+    setErrorStyles(request, "Please describe your request");
+  } else {
+    removeErrorStyles(request);
+  }
+
+  // console.log("Form data:");
+  // console.log("First Name:", firstName.value);
+  // console.log("Last Name:", lastName.value);
+  // console.log("Email:", email.value);
+  // console.log("Contact Number:", contactNumber.value);
+  // console.log("Request:", request.value);
+});
 
 var scene = document.getElementById("contactScene");
-var parallaxInstance = new Parallax(scene);
+// var parallaxInstance = new Parallax(scene);
