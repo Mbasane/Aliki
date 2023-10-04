8e44393ea16b908376e80a1eb81e09feb9faf9ea
Vuyani Daweti <vuyanidwt@gmail.com>
2023-10-04 16:59:30 +0200

    new PHP form

---
+++ sendingform.php
@@ -0,0 +1,16 @@
+<?php 
+echo "Sending mail "
+// $recipient = "misokuhle46@gmail.com";
+// $sender=$_POST["emailAddress"];
+// $firstName=$_POST["firstName"];
+// $lastName=$_POST["lastName"];
+// $description=$_POST["description"];
+// $fullName= $firstName+ " "+$lastName;
+// $contactNumber = $_POST["contactNumber"];
+// $subject ="Service request by: ".$firstName .$lastName;
+// $message ="Contact us message {$description} \n Contact Number:{$contactNumber}";
+// $header = "From: ".$fullName;
+// $success=mail($recipient,$subject,$message,$header);
+// echo $success;
+// header("Location:localhost/index.html");
+?>
